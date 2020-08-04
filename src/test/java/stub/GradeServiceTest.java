a41ca71fd3391e4a734b4d82b10a8647c64f26fe
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-04 21:58:43 +0800

    test2

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,12 +1,18 @@
 package stub;
 import mock.SecurityCenter;
+import org.apiguardian.api.API;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 
@@ -14,17 +20,18 @@ public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    //@InjectMocks
+    //private GradeService gradeService;
     @Mock
-    private GradeSystem mockGradeSystem;
-    @InjectMocks
-    private GradeService gradeService;
+    private GradeService mockGradeService;
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        securityCenter.switchOn();
-        gradeService.calculateAverageGrades(90.0,95.0,100.0);
+        GradeSystem gradeSystem = new GradeSystem();
+        long studentID = 1;
+        when(mockGradeService.calculateAverageGrades(studentID)).thenReturn(90.0);
 
-
-        verify(mockDoorPanel).close();
-        //Assertions.assertEquals(90.0, result);
+//        verify(mockGradeService).calculateAverageGrades(studentID);
+        Assertions.assertEquals(90.0,mockGradeService.calculateAverageGrades(studentID));
     }
 }
